0a92a6a796f1cb18e9c41381ff3e34841dc72da3
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2022-02-09 22:32:44 +0530

    UI: Profile page is done

--- script.js
+++ script.js
@@ -11,53 +11,6 @@ function logout() {
     window.location.href = "signin.html";
 }
 
-// function addTask() {
-//     let task = document.getElementById("task").value;
-
-//     let task_records = new Array();
-//     task_records = JSON.parse(localStorage.getItem("taskList"))
-//         ? JSON.parse(localStorage.getItem("taskList"))
-//         : [];
-
-//     if (
-//         task_records.some((item) => {
-//             return item.task == task;
-//         })
-//     ) {
-//         alert("Duplicate Task");
-//     } else {
-//         task_records.push({
-//             task: task,
-//             status: 0,
-//         });
-//         localStorage.setItem("taskList", JSON.stringify(task_records));
-//     }
-//     showTask();
-// }
-
-// function showTask() {
-//     let task_list;
-//     if (document.getElementById("taskList") != null) {
-//         task_list = document.getElementById("taskList").innerHTML;
-//     }
-
-//     let task_records = new Array();
-//     task_records = JSON.parse(localStorage.getItem("taskList"))
-//         ? JSON.parse(localStorage.getItem("taskList"))
-//         : [];
-//     console.log(task_records);
-
-//     if (task_records) {
-//         for (let i = 0; i < task_records.length; i++) {
-//             let addDiv = document.createElement("div");
-//             addDiv.className = "task";
-//             // addDiv.innerHTML = `<div class="task-text">${task_records[i].status}</div><div class="task-actions"><img src="https://img.icons8.com/ios-glyphs/30/ffffff/check-all.png" /> <img src="https://img.icons8.com/ios-glyphs/30/ffffff/filled-trash.png" /></div>`;
-//             addDiv.innerHTML = "<h1>Hello World</h1>";
-//             task_list.appendChild(addDiv);
-//         }
-//     }
-// }
-
 const inputValue = document.getElementById("task");
 const addTaskBtn = document.getElementsByClassName("addButton")[0];
 let taskItems;
